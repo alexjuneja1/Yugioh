4770f9bb4659cef9e5ff42f3f4dad16d60585f7f
Alexander Juneja <alexjuneja1@gmail.com>
2015-11-16 12:56:22 -0800

    added 3 HTML pages, about contact and index. also modified app.js to reach these pages, as added a functioning header and footer for the page.

--- app.js
+++ app.js
@@ -11,8 +11,37 @@ app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
-app.get('/' function(req,res){
-  res.send('This is our Cafe list')
+app.get('/', function(req,res){
+  var collection = [
+  {name: "Cafe Hasbro", address: "12 West Street"},
+  {name: "Cafe Fisher Price", address: "80 North Avenue"},
+  {name: "Cafe Bandai", address: "99 South Boulevard"}
+]
+  var data = { title: "The Home Page",
+              message: "The Cafe Lists",
+              cafes: collection
+  }
+  res.render('index', data)
+})
+
+app.get('/about', function(req,res){
+  var data = { title: "About Us",
+              message: "Eddie and his boys Seri and Alex are some cool guys with attitudes who talk about cafes on a Yu-Gi-Oh! page."
+            }
+  res.render('about', data)
+})
+
+app.get('/contact', function(req,res){
+  var collection = [
+    {name: "Eddie Jung", email: "EddieTheMan@bro.com"},
+    {name: "Seri Orfali", email: "SeriMasterCoder@coolman.com"},
+    {name: "Alex Juneja", email: "AlexDude@guy.edu"}
+  ]
+  var data = { title: "Contact Us",
+              message: "Feel free to contact us by e-mail!",
+              users: collection
+            }
+  res.render('contact', data)
 })
 
 var cafeRoutes = require('./routes/cafeRoutes.js')
